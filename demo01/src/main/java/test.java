bec286086353a6fcf9cbed2c1bb3fa220b7856c4
JL-20191210 <534457091@qq.com>
2022-03-15 16:43:29 +0800

    test

---
+++ demo01/src/main/java/test.java
@@ -0,0 +1,17 @@
+/**
+ * @author 胡金龙
+ * @create 2022-03-15 16:22
+ */
+public class test {
+    public static void main(String[] args) {
+       for (int i = 1; i<10; i++){
+           if(i==5){
+               System.out.println("i=5");
+           }else if(i==6){
+               System.out.println("i=6");
+           }else{
+               System.out.println(i);
+           }
+       }
+    }
+}
